029cdb34b41d281046380ed54f71c371cb38d35e
Sharon Paisner <spaisner@fitbit.com>
2020-12-01 16:45:28 -0800

    short circuit on part 2

--- 2020/day_01/report_repair.py
+++ 2020/day_01/report_repair.py
@@ -7,18 +7,25 @@ entry_set = set()
 for entry in entries:
     inverse = 2020 - entry
     if inverse in entry_set:
-        print("(Day 1, Part 1) sum of %d and %d is %d" % (entry, inverse, entry * inverse))
+        print("(Day 1, Part 1) %d + %d = 2020, product is %d" % (entry, inverse, entry * inverse))
         break
     else:
         entry_set.add(entry)
 
 
 # finding 3 entries that add to 2020 is harder, hmm...
-#entries.sort(reverse=True)
+end = False
 for i in range(0,len(entries)):
     for j in range(1, len(entries)):
         for k in range(2, len(entries)):
             val = entries[i] + entries[j] + entries[k]
             if val == 2020:
-                print("(Day 1, Part 2) sum of %d and %d and %d is %d" % (entries[i], entries[j], entries[k], entries[i] * entries[j] * entries[k]))
+                print("(Day 1, Part 2) %d + %d + %d = 2020, product is %d" % (entries[i], entries[j], entries[k], entries[i] * entries[j] * entries[k]))
+                end = True
+                break
+
+        if end:
+            break
+    if end:
+        break
 
