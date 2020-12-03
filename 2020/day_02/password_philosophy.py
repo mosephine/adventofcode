5c4ae005167b48231ffd9925f77ce17c8c3e1659
Sharon Paisner <spaisner@fitbit.com>
2020-12-03 11:36:01 -0800

    completed days 2 and 3

---
+++ 2020/day_02/password_philosophy.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+
+import re
+
+lines = [line.rstrip('\n') for line in open('data.in')]
+
+rule_re = re.compile("(\d+)-(\d+) (\w): (\w+)")
+# parse input into passwords array
+passwords = []
+for line in lines:
+    rule_match = rule_re.search(line)
+    if rule_match:
+        password = [int(rule_match.group(1)), int(rule_match.group(2)), rule_match.group(3), rule_match.group(4)]
+        passwords.append(password)
+
+# count the valid passwords according to Part 1 rules
+valid_count = 0
+for password in passwords:
+    count = password[3].count(password[2])
+    if count >= password[0] and count <= password[1]:
+        valid_count += 1
+
+print("(Day 2, Part 1) number of valid passwords: " + str(valid_count))
+
+# count the valid passwords according to Part 2 rules
+valid_count = 0
+for password in passwords:
+    found = False
+    if password[2] == password[3][password[0] - 1]:
+        found = True
+
+    if len(password[3]) >= password[1]:
+        if password[2] == password[3][password[1] - 1]:
+            if not found:
+                valid_count += 1
+        elif found:
+            valid_count += 1
+
+print("(Day 2, Part 2) number of valid passwords: " + str(valid_count))
---
+++ 2020/day_03/toboggan_trajectory.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+
+lines = [line.rstrip('\n') for line in open('data.in')]
+
+# let's count some trees
+def count_trees(slope_x, slope_y, matrix):
+    pos_x = 0
+    pos_y = 0
+    len_x = len(matrix[0])
+    len_y = len(matrix)
+
+    trees = 0
+    while pos_y < len_y:
+        if matrix[pos_y][pos_x] == '#':
+            trees += 1
+
+        pos_y += slope_y
+        pos_x = (pos_x + slope_x) % len_x
+
+    return trees
+
+trees = count_trees(3, 1, lines)
+print("(Day 3, Part 1) number of trees hit on our way down: " + str(trees))
+
+# slopes to check
+slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+total = 1
+for slope in slopes:
+    total *= count_trees(slope[0], slope[1], lines)
+
+print("(Day 3, Part 2) product of trees hit on all slopes: " + str(total))
